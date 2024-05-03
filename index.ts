445d289b5cbcd7fed6c406d7fad1dd5cbd9a0157
iqra11748 <164212492+iqra11748@users.noreply.github.com>
2024-05-04 02:02:21 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,12 @@
+// marksheet
+let percentage = 60;
+
+if (percentage >= 80 && percentage <= 100) {
+    console.log("A+")
+} else if (percentage >= 70 && percentage <= 89) {
+    console.log("A")
+} else if (percentage >=60 && percentage <= 65) {
+    console.log("B")
+} else {
+    console.log("Fail")
+}
